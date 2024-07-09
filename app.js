fa629edd7bbaaedcfd2655d398ce5ba2f184fd80
Vishal Yadav <144472297+vishalyadav2000@users.noreply.github.com>
2024-07-10 04:10:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+document.addEventListener('DOMContentLoaded', (event) => {
+  const playButton = document.getElementById('play-button');
+  
+  playButton.addEventListener('click', () => {
+    window.location.href = 'game.html';
+  });
+});
